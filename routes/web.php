1a3617c99069dd5f3cd32776581fe764bc6a83ff
Nicolas <nbq@protonmail.com>
2020-01-17 17:34:14 +0100

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 Route::middleware('auth')->get('/v/{any?}', function (){
     return view('vue');
 })->where('any', '[\/\w\.-]*');
-
 */
 
 Auth::routes();
@@ -24,4 +23,3 @@ Route::get('/', 'IndexController@index');
 
 // HomeController->auth
 Route::middleware('auth')->get('/home', 'HomeController@index');
-
